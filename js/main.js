b1ea550d196f412fb79309243e5dcf6db4089ccc
F1rman <ihorkharchyshyn@gmail.com>
2020-07-22 17:39:40 +0300

    weather + clock + tasks + local stor

--- js/main.js
+++ js/main.js
@@ -4,19 +4,60 @@ var app = angular.module('Artmoney', ['ngAnimate', 'ngMaterial']);
 app.config(['$compileProvider', function($compileProvider) {
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
 }]);
-app.controller('Global', function($scope, $rootScope, $location,$mdDialog) {
+app.controller('Global', function($scope, $rootScope, $location, $mdDialog, $interval) {
+  var stor = chrome.storage.sync;
 
-  this.data = {
-    tasks:new Array(),
-    pushNew: function(){
-      console.log(this.tasks);
-      this.tasks.push($scope.task)
+  $scope.tasks = new Array()
+  stor.get(null, function(items) {
+    console.log(items.tasks);
+    $scope.tasks = (items.tasks)
+    console.log($scope.tasks);
+  });
+  console.log($scope.tasks);
+  $scope.data = {
+    pushNew: function() {
+      $scope.tasks.push($scope.task)
       $scope.task = ''
+      console.log($scope.tasks);
+      stor.set({
+        'tasks': $scope.tasks
+      }, function() {
+        console.log('Settings saved');
+      });
     },
-    removeItem:function(index){
-      console.log(this.tasks);
-    this.tasks.splice(index, 1);
-  },
+    removeItem: function(index) {
+      console.log(index);
+      console.log($scope.tasks);
+      $scope.tasks.splice(index, 1);
+      stor.set({
+        'tasks': $scope.tasks
+      }, function() {
+        console.log('Settings saved');
+      });
+    },
   }
-  this.a = 'ads'
+
+
+
+
+  // ЧАС і ДАТА
+  $scope.date = new Date()
+  setInterval(function() {
+    $scope.date = new Date()
+    $scope.$apply()
+  }, 1000);
+
+
+  navigator.geolocation.getCurrentPosition(function(loc) {
+    $scope.latitude = loc.coords.latitude;
+    $scope.longitude = loc.coords.longitude;
+    console.log($scope.latitude);
+
+  });
+  setTimeout(function() {
+    $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + Math.round($scope.latitude) + '&lon=' + Math.round($scope.longitude) + '&appid=91d758bf91a800494495c0d2eec1ce73', function(data) {
+      $scope.weather = data
+    });
+  }, 10);
+
 })
