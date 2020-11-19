5298c1aba17dc470139c0b3353a32e36055c41c4
F1rman <ihorkharchyshyn@gmail.com>
2020-11-19 13:09:49 +0200

    bubles + search + img change

--- js/main.js
+++ js/main.js
@@ -1,106 +1,325 @@
-var app = angular.module('Artmoney', ['ngAnimate', 'ngMaterial']);
 
 
-app.config(['$compileProvider', function($compileProvider) {
-  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
-}]);
-app.controller('Global', function($scope, $rootScope, $location, $mdDialog, $interval) {
-  var stor = chrome.storage.sync;
-  $(document).ready(() => {
-    var min = 1;
-    var max = 999;
-    var mySwiper = new Swiper('.swiper-container', {
-      autoplay: {
-        delay: 30000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    var random = Math.floor(Math.random() * (+max - +min)) + +min;
-    mySwiper.appendSlide('<div class="swiper-slide" ><img   id="img' + random + '" src="https://picsum.photos/id/' + random + '/1920"   alt=""></div>');
-    $("#img" + random + "").on("error", function() {
-      $(this).attr("src", "https://picsum.photos/1920");
-    });
-    $scope.newSlide = () => {
-      var random = Math.floor(Math.random() * (+max - +min)) + +min;
-      mySwiper.appendSlide('<div class="swiper-slide" ><img   id="img' + random + '" src="https://picsum.photos/id/' + random + '/1920"   alt=""></div>');
-      $("#img" + random + "").on("error", function() {
-        $(this).attr("src", "https://picsum.photos/1920");
-      });
-      console.log(mySwiper.slides);
-      setTimeout(function() {
-        mySwiper.removeSlide(0);
-      }, 300);
-    }
+$(document).ready(()=>{
+  var sound;
+$('#search').keyup(()=>{
+  if ($('#search').val()!='') {
+    $('.ico_clear').addClass('active')
+  }
+  else {
+    $('.ico_clear').removeClass('active')
 
-    document.querySelector('.swiper-button-next').addEventListener('click', function(e) {
-      e.preventDefault();
+  }
+})
 
-      $scope.newSlide()
-    });
-
+  $('.ico_clear').click(()=>{
+      $('.ico_clear').removeClass('active')
+    $('#search').val('')
   })
+  $('.ico_search').click(()=>{
+  $('form.search').submit();
+  })
+  var coutn = 1;
+  $('.change_bg').click(()=>{
+    $('.change_bg').addClass('rotate')
+    coutn++
+    var img = new Image();
+    $(img).one('load', function() {
+      $('.change_bg').removeClass('rotate')
+      $('.change_bg').addClass('rotate_start')
 
-  $scope.tasks = new Array()
-  stor.get(null, function(items) {
-    console.log(items.tasks);
-    $scope.tasks = (items.tasks)
-    console.log($scope.tasks);
-  });
-  console.log($scope.tasks);
-  $scope.data = {
-    pushNew: function() {
-      $scope.tasks.push($scope.task)
-      $scope.task = ''
-      console.log($scope.tasks);
-      stor.set({
-        'tasks': $scope.tasks
-      }, function() {
-        console.log('Settings saved');
-      });
-    },
-    removeItem: function(index) {
-      console.log(index);
-      console.log($scope.tasks);
-      $scope.tasks.splice(index, 1);
-      stor.set({
-        'tasks': $scope.tasks
-      }, function() {
-        console.log('Settings saved');
-      });
-    },
+    });
+    img.src ='https://source.unsplash.com/1920x1080/?nature,water'
+    $('.image').addClass('blur_down')
+      // $('.bg'+(coutn-2)).remove()
+      $('.image').removeClass('blur_down')
+      $(img).addClass('bg bg'+coutn)
+      $('.image').append(img)
+})
+
+
+  // ---------
+  // Functions
+  // ---------
+
+  var canvas = document.querySelector('canvas');
+      canvas.width = document.body.clientWidth;
+      canvas.height = document.body.clientHeight;
+  var ctx = canvas.getContext('2d');
+  var count = canvas.height;
+  var bubbles = [];
+  var bubbleCount = 20;
+  var bubbleSpeed = 1;
+  var popLines = 6;
+  var popDistance = 40;
+  var mouseOffset = {
+    x: 0,
+    y: 0
   }
 
 
 
+  // --------------
+  // Animation Loop
+  // --------------
 
-  // ЧАС і ДАТА
-  $scope.date = new Date()
-  setInterval(function() {
-    $scope.date = new Date()
-    $scope.$apply()
-  }, 1000);
+  function animate() {
 
-  //Location
-  navigator.geolocation.getCurrentPosition(function(loc) {
-    $scope.latitude = loc.coords.latitude;
-    $scope.longitude = loc.coords.longitude;
+
+
+    // ------------
+    // Clear Canvas
+    // ------------
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+
+    // ------------
+    // Draw Bubbles
+    // ------------
+
+    ctx.beginPath();
+    for (var i = 0; i < bubbles.length; i++) {
+      // first num = distance between waves
+      // second num = wave height
+      // third num = move the center of the wave away from the edge
+      bubbles[i].position.x = Math.sin(bubbles[i].count/bubbles[i].distanceBetweenWaves) * 50 + bubbles[i].xOff;
+      bubbles[i].position.y = bubbles[i].count;
+      bubbles[i].render();
+
+      if(bubbles[i].count < 0 - bubbles[i].radius) {
+        bubbles[i].count = canvas.height + bubbles[i].yOff;
+      } else {
+        bubbles[i].count -= bubbleSpeed;
+      }
+    }
+
+    // ---------------
+    // On Bubble Hover
+    // ---------------
+    for (var i = 0; i < bubbles.length; i++) {
+      if(mouseOffset.x > bubbles[i].position.x - bubbles[i].radius && mouseOffset.x < bubbles[i].position.x + bubbles[i].radius) {
+        if(mouseOffset.y > bubbles[i].position.y - bubbles[i].radius && mouseOffset.y < bubbles[i].position.y + bubbles[i].radius) {
+          buble_sound()
+          for (var a = 0; a < bubbles[i].lines.length; a++) {
+            popDistance = bubbles[i].radius * 0.5;
+            bubbles[i].lines[a].popping = true;
+            bubbles[i].popping = true;
+
+          }
+        }
+      }
+
+    }
+
+
+    window.requestAnimationFrame(animate);
+  }
+
+  window.requestAnimationFrame(animate);
+
+
+
+  // ------------------
+  // Bubble Constructor
+  // ------------------
+
+  var createBubble = function() {
+    this.position = {x: 0, y: 0};
+    this.radius = 20 + Math.random() * 6;
+    this.xOff = Math.random() * canvas.width - this.radius;
+    this.yOff = Math.random() * canvas.height;
+    this.distanceBetweenWaves = 50 + Math.random() * 40;
+    this.count = canvas.height + this.yOff;
+    this.color = '#8bc9ee';
+    this.lines = [];
+    this.popping = false;
+    this.maxRotation = 85;
+    this.rotation = Math.floor(Math.random() * (this.maxRotation - (this.maxRotation * -1))) + (this.maxRotation * -1);
+    this.rotationDirection = 'forward';
+
+    // Populate Lines
+    for (var i = 0; i < popLines; i++) {
+      var tempLine = new createLine();
+          tempLine.bubble = this;
+          tempLine.index = i;
+
+      this.lines.push(tempLine);
+    }
+
+    this.resetPosition = function() {
+      this.position = {x: 0, y: 0};
+      this.radius = 8 + Math.random() * 6;
+      this.xOff = Math.random() * canvas.width - this.radius;
+      this.yOff = Math.random() * canvas.height;
+      this.distanceBetweenWaves = 50 + Math.random() * 40;
+      this.count = canvas.height + this.yOff;
+      this.popping = false;
+
+    }
+
+    // Render the circles
+    this.render = function() {
+      if(this.rotationDirection === 'forward') {
+        if(this.rotation < this.maxRotation) {
+          this.rotation++;
+        } else {
+          this.rotationDirection = 'backward';
+        }
+      } else {
+        if(this.rotation > this.maxRotation * -1) {
+          this.rotation--;
+        } else {
+          this.rotationDirection = 'forward';
+        }
+      }
+
+      ctx.save();
+      ctx.translate(this.position.x, this.position.y);
+      ctx.rotate(this.rotation*Math.PI/180);
+
+      if(!this.popping) {
+        ctx.beginPath();
+        ctx.strokeStyle = '#8bc9ee';
+        ctx.lineWidth = 1;
+        ctx.arc(0, 0, this.radius - 3, 0, Math.PI*1.5, true);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(0, 0, this.radius, 0, Math.PI*2, false);
+        ctx.stroke();
+
+      }
+
+
+      ctx.restore();
+
+      // Draw the lines
+      for (var a = 0; a < this.lines.length; a++) {
+        if(this.lines[a].popping) {
+          if(this.lines[a].lineLength < popDistance && !this.lines[a].inversePop) {
+            this.lines[a].popDistance += 0.06;
+          } else {
+            if(this.lines[a].popDistance >= 0) {
+              this.lines[a].inversePop = true;
+              this.lines[a].popDistanceReturn += 1;
+              this.lines[a].popDistance -= 0.03;
+            } else {
+              this.lines[a].resetValues();
+              this.resetPosition();
+            }
+          }
+
+          this.lines[a].updateValues();
+          this.lines[a].render();
+        }
+      }
+    }
+  }
+
+
+
+  // ----------------
+  // Populate Bubbles
+  // ----------------
+
+  for (var i = 0; i < bubbleCount; i++) {
+    var tempBubble = new createBubble();
+
+    bubbles.push(tempBubble);
+  }
+
+
+
+  // ----------------
+  // Line Constructor
+  // ----------------
+
+  function createLine() {
+    this.lineLength = 0;
+    this.popDistance = 0;
+    this.popDistanceReturn = 0;
+    this.inversePop = false; // When the lines reach full length they need to shrink into the end position
+    this.popping = false;
+
+    this.resetValues = function() {
+      this.lineLength = 0;
+      this.popDistance = 0;
+      this.popDistanceReturn = 0;
+      this.inversePop = false;
+      this.popping = false;
+      this.updateValues();
+    }
+
+    this.updateValues = function() {
+      this.x = this.bubble.position.x + (this.bubble.radius + this.popDistanceReturn) * Math.cos(2 * Math.PI * this.index / this.bubble.lines.length);
+      this.y = this.bubble.position.y + (this.bubble.radius + this.popDistanceReturn) * Math.sin(2 * Math.PI * this.index / this.bubble.lines.length);
+      this.lineLength = this.bubble.radius * this.popDistance;
+      this.endX = this.lineLength;
+      this.endY = this.lineLength;
+    }
+
+    this.render = function() {
+      this.updateValues();
+
+      ctx.beginPath();
+      ctx.strokeStyle = '#8bc9ee';
+      ctx.lineWidth = 2;
+      ctx.moveTo(this.x, this.y);
+      if(this.x < this.bubble.position.x) {
+        this.endX = this.lineLength * -1;
+      }
+      if(this.y < this.bubble.position.y) {
+        this.endY = this.lineLength * -1;
+      }
+      if(this.y === this.bubble.position.y) {
+        this.endY = 0;
+      }
+      if(this.x === this.bubble.position.x) {
+        this.endX = 0;
+      }
+      ctx.lineTo(this.x + this.endX, this.y + this.endY);
+      ctx.stroke();
+    };
+  }
+
+
+
+  // ---------------
+  // Event Listeners
+  // ---------------
+
+  canvas.addEventListener('mousemove', mouseMove);
+
+  function mouseMove(e) {
+    mouseOffset.x = e.offsetX;
+    mouseOffset.y = e.offsetY;
+
+  }
+
+  window.addEventListener('resize', function() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
   });
-  // weather
-  setTimeout(function() {
-    $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + Math.round($scope.latitude) + '&lon=' + Math.round($scope.longitude) + '&units=metric&appid=91d758bf91a800494495c0d2eec1ce73', function(data) {
-      $scope.weather = data;
-      $scope.city = $scope.weather.name;
-      $scope.temp = Math.round($scope.weather.main.temp);
-      $scope.cloud = $scope.weather
-    });
-  }, 10);
 
+
+  // ---------------
+  // Event Listeners
+  // ---------------
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення
+
+  }
+  var dd;
+  var buble_sound = ()=>{
+    clearTimeout(dd)
+   dd =   setTimeout(function () {
+    sound = new Audio('mp3/buble'+getRandomInt(1,3)+'.mp3')
+    sound.volume = (0.2)
+    sound.play()
+    }, 30);
+    }
 })
